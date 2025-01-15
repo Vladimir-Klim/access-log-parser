07a0abc09a2ddf49505ea2a878ef43a7c6e920b8
Vladimir-Klim <bobatich1@gmail.com>
2025-01-15 22:42:42 +0700

    Добавлена логика в которой в консоли будет запрашиваться путь к файлу и проверятся его корректность

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        var PATH_ERROR = "Введен ошибочный путь к файлу или путь к несуществующему файлу.";
+        var count = 0;
+        while (true) {
+            var path = new Scanner(System.in).nextLine();
+            var file = new File(path);
+            var fileExists = file.exists();
+            var isDirectory = file.isDirectory();
+            if (!fileExists) {
+                System.out.println(PATH_ERROR);
+                continue;
+            }
+            if (isDirectory) {
+                System.out.println(PATH_ERROR);
+            } else {
+                System.out.printf("Путь указан верно. Это файл номер %d%n", ++count);
+            }
+        }
     }
 }
