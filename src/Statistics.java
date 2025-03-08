1ff31ce5610bce635ee055752c4fd13a4212cb9d
Vladimir-Klim <bobatich1@gmail.com>
2025-03-09 02:25:18 +0700

    В классе Statistics добавлен метод, который возвращает список всех существующих страниц сайта. В классе Statistics добавлен метод, который возвращает статистику операционных систем пользователей сайта.

--- src/Statistics.java
+++ src/Statistics.java
@@ -1,18 +1,21 @@
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Statistics {
     private long totalTraffic;
     private LocalDateTime minTime;
     private LocalDateTime maxTime;
     private List<LogEntry> entries;
+    private Set<String> existingPages;
+    private Map<String, Integer> osFrequency;
 
     public Statistics() {
         this.totalTraffic = 0;
         this.entries = new ArrayList<>();
         this.minTime = LocalDateTime.MAX;
         this.maxTime = LocalDateTime.MIN;
+        this.existingPages = new HashSet<>();
+        this.osFrequency = new HashMap<>();
     }
 
     public void addEntry(LogEntry entry) {
@@ -25,6 +28,11 @@ public class Statistics {
         if (entry.getTime().isAfter(maxTime)) {
             maxTime = entry.getTime();
         }
+        if (entry.getResponseCode() == 200) {
+            existingPages.add(entry.getPath());
+        }
+        String os = entry.getAgent().getOS();
+        osFrequency.put(os, osFrequency.getOrDefault(os, 0) + 1);
     }
 
     public double getTrafficRate() {
@@ -45,5 +53,25 @@ public class Statistics {
 
     public LocalDateTime getMaxTime() {
         return maxTime;
+    }
+
+    public Set<String> getExistingPages() {
+        return existingPages;
+    }
+
+    public Map<String, Double> getOsStatistics() {
+        Map<String, Double> osStats = new HashMap<>();
+
+        int totalOsCount = 0;
+        for (int count : osFrequency.values()) {
+            totalOsCount += count;
+        }
+
+        for (Map.Entry<String, Integer> entry : osFrequency.entrySet()) {
+            double percentage = (double) entry.getValue() / totalOsCount;
+            osStats.put(entry.getKey(), percentage);
+        }
+
+        return osStats;
     }
 }
